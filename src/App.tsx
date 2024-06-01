984cac3a07667ea3578777389fad663b120b35e1
songforthemute <huni0423@naver.com>
2024-06-01 17:18:59 +0900

    To do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, FormEvent, useState } from "react";
+import "./App.css";
+import ToDoItem from "./components/ToDoItem";
+
+export enum EStatus {
+    WAITING = "WAITING",
+    PROGRESS = "PROGRESS",
+    COMPLETE = "COMPLETE",
+}
+
+export interface IToDoItem {
+    title: string;
+    desc: string;
+    status?: EStatus;
+}
+
+type TToDoList = IToDoItem[];
+
+enum EInputType {
+    title = "title",
+    desc = "desc",
+    status = "status",
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [toDoList, setToDoList] = useState<TToDoList>([]);
+
+    const [toDo, setToDo] = useState<IToDoItem>({
+        title: "",
+        desc: "",
+    });
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        setToDoList((prev) => [toDo, ...prev]);
+        setToDo({ title: "", desc: "", status: undefined });
+    };
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>, type: EInputType) => {
+        const { value } = e.currentTarget;
+
+        setToDo((prev) => ({ ...prev, [type]: value }));
+    };
+
+    return (
+        <div className="App">
+            <header>
+                <form onSubmit={onSubmit}>
+                    <input
+                        value={toDo.title}
+                        onChange={(e) => onChange(e, EInputType.title)}
+                        placeholder="Title"
+                    />
+                    <input
+                        value={toDo.desc}
+                        onChange={(e) => onChange(e, EInputType.desc)}
+                        placeholder="Description"
+                    />
+                    <select
+                        value={toDo.status}
+                        onChange={(e) =>
+                            onChange(
+                                e as unknown as ChangeEvent<HTMLInputElement>,
+                                EInputType.status
+                            )
+                        }
+                    >
+                        <option value={undefined}>Select Status</option>
+                        <option value={EStatus.WAITING}>
+                            {EStatus.WAITING}
+                        </option>
+                        <option value={EStatus.PROGRESS}>
+                            {EStatus.PROGRESS}
+                        </option>
+                        <option value={EStatus.COMPLETE}>
+                            {EStatus.COMPLETE}
+                        </option>
+                    </select>
+                    <button type="submit">Submit</button>
+                </form>
+            </header>
+            <main>
+                <ul>
+                    {toDoList.map((item, index) => {
+                        return (
+                            <ToDoItem
+                                key={index}
+                                title={item.title}
+                                desc={item.desc}
+                                status={item.status}
+                            />
+                        );
+                    })}
+                </ul>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ToDoItem.tsx
@@ -0,0 +1,32 @@
+import { EStatus, IToDoItem } from "../App";
+
+const ToDoItem = ({ title, desc, status = EStatus.WAITING }: IToDoItem) => {
+    const statusIcon =
+        status === EStatus.WAITING
+            ? "👀"
+            : status === EStatus.PROGRESS
+            ? "🔥"
+            : "✅";
+
+    return (
+        <li
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "10px",
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                marginBottom: "10px",
+            }}
+        >
+            <span>{statusIcon}</span>
+            <div>
+                <h3>{title}</h3>
+                <p>{desc}</p>
+            </div>
+        </li>
+    );
+};
+
+export default ToDoItem;

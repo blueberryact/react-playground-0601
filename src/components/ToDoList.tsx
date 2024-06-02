797a097a0755413e76b9930e2b7da6b10165c947
sjndowair <wodnjsdl1125@naver.com>
2024-06-02 18:52:07 +0900

    fix: App 컴포넌트를 ToDoList 컴포넌트 내부에 정의하고 반환 부분 수정 및 input type 정의 수정

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,57 +1,53 @@
 import { useState, ChangeEvent } from "react";
 
 function ToDoList() {
-    function App() {
-        const [name, setName] = useState<string>("");
-        const [gender, setGender] = useState<string>("");
-        const [birth, setBirth] = useState<string>("");
-        const [bio, setBio] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [gender, setGender] = useState<string>("");
+    const [birth, setBirth] = useState<string>("");
+    const [bio, setBio] = useState<string>("");
 
-        const onChangeName = (e: ChangeEvent<HTMLInputElement>) => {
-            setName(e.target.value);
-            console.log(e.target.value);
-        };
-        const onChangeGender = (e: ChangeEvent<HTMLSelectElement>) => {
-            setGender(e.target.value);
-            console.log(e.target.value);
-        };
-        const onChangeBirth = (e: ChangeEvent<HTMLInputElement>) => {
-            setBirth(e.target.value);
-            console.log(e.target.value);
-        };
-        const onChangeBio = (e: ChangeEvent<HTMLTextAreaElement>) => {
-            setBio(e.target.value);
-            console.log(e.target.value);
-        };
+    const onChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+        console.log(e.target.value);
+    };
+    const onChangeGender = (e: ChangeEvent<HTMLSelectElement>) => {
+        setGender(e.target.value);
+        console.log(e.target.value);
+    };
+    const onChangeBirth = (e: ChangeEvent<HTMLInputElement>) => {
+        setBirth(e.target.value);
+        console.log(e.target.value);
+    };
+    const onChangeBio = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setBio(e.target.value);
+        console.log(e.target.value);
+    };
 
-        return (
-            <>
-                <div>
-                    <input
-                        value={name}
-                        onChange={onChangeName}
-                        placeholder="이름"
-                        type=" "
-                    />
-                </div>
-                <div>
-                    <select value={gender} onChange={onChangeGender}>
-                        <option key=""></option>
-                        <option key="남성">남성</option>
-                        <option key="여성">여성</option>
-                    </select>
-                </div>
-                <div>
-                    <input type="date" value={birth} onChange={onChangeBirth} />
-                </div>
-                <div>
-                    <textarea value={bio} onChange={onChangeBio}></textarea>
-                </div>
-            </>
-        );
-    }
-
-    return <App />;
+    return (
+        <>
+            <div>
+                <input
+                    value={name}
+                    onChange={onChangeName}
+                    placeholder="이름"
+                    type="text"
+                />
+            </div>
+            <div>
+                <select value={gender} onChange={onChangeGender}>
+                    <option value=""></option>
+                    <option value="남성">남성</option>
+                    <option value="여성">여성</option>
+                </select>
+            </div>
+            <div>
+                <input type="date" value={birth} onChange={onChangeBirth} />
+            </div>
+            <div>
+                <textarea value={bio} onChange={onChangeBio}></textarea>
+            </div>
+        </>
+    );
 }
 
 export default ToDoList;

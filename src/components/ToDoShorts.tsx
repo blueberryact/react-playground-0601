e9ca786893a5faa38994cc6344743bc71f94cff0
sjndowair <wodnjsdl1125@naver.com>
2024-06-02 18:52:07 +0900

    fix: App 컴포넌트를 ToDoList 컴포넌트 내부에 정의하고 반환 부분 수정 및 input type 정의 수정

--- src/components/ToDoShorts.tsx
+++ src/components/ToDoShorts.tsx
@@ -7,57 +7,55 @@ function ToDoShorts() {
         birth: "",
         bio: "",
     });
-
     const handleOnChange = (
         e: ChangeEvent<
-            HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+            | HTMLInputElement
+            | HTMLSelectElement
+            | HTMLSelectElement
+            | HTMLTextAreaElement
         >
     ) => {
-        console.log("현재 수정 대상:", e.currentTarget.name);
-        console.log("수정값:", e.currentTarget.value);
+        console.log("현재 수정 대상", e.target.name);
+        console.log("수정값", e.target.value);
         setState({
             ...state,
-            [e.currentTarget.name]: e.currentTarget.value,
+            [e.target.name]: e.target.value,
         });
     };
-
     return (
         <div>
             <div>
                 <input
+                    onChange={handleOnChange}
                     name="name"
                     value={state.name}
-                    onChange={handleOnChange}
                     placeholder="이름"
                 />
             </div>
             <div>
                 <select
                     name="gender"
-                    value={state.name}
+                    value={state.gender}
                     onChange={handleOnChange}
                 >
-                    <option key=""></option>
-                    <option key="남성"></option>
-                    <option key="여성"></option>
+                    <option value={""}></option>
+                    <option value={"남성"}>남성</option>
+                    <option value={"여성"}>여성</option>
                 </select>
             </div>
             <div>
                 <input
-                    name="birth"
-                    type="date"
                     value={state.birth}
+                    type="date"
+                    name="birth"
                     onChange={handleOnChange}
                 />
             </div>
-            <div>
-                <textarea
-                    name="bio"
-                    id=""
-                    value={state.bio}
-                    onChange={handleOnChange}
-                ></textarea>
-            </div>
+            <textarea
+                name="bio"
+                onChange={handleOnChange}
+                value={state.bio}
+            ></textarea>
         </div>
     );
 }

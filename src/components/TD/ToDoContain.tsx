1a2587271b52a2e6992200bec95ed4ffbebfb2c9
sjndowair <wodnjsdl1125@naver.com>
2024-06-08 18:52:11 +0900

    delete: 불필요코드 삭제

--- src/components/TD/ToDoContain.tsx
+++ src/components/TD/ToDoContain.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import styled from "styled-components";
-import ToDoContain from "./ToDo";
 import { TODOFC } from "../../types/type";
 
 const ToDoContainer: TODOFC = ({ children }) => {

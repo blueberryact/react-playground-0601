9f7416a148bf054fd7e329fe54a75c26d46d1af1
sjndowair <wodnjsdl1125@naver.com>
2024-06-08 18:53:05 +0900

    add: ToDoItem 수정 변경(gpt사용)

--- src/components/TD/ToDoSearch.tsx
+++ src/components/TD/ToDoSearch.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
+import ToDoItemBox from "./ToDoItemBox";
 
 const Searchbar = styled.input`
     margin-bottom: 20px;
@@ -12,11 +13,58 @@ const Searchbar = styled.input`
     box-shadow: 0px 1px 1px 0px;
 `;
 
-const ToDoSearch = () => {
+const DivContain = styled.div`
+    background-color: rgba(10, 10, 10, 0.2);
+    margin-bottom: 1.5rem;
+    border-bottom: 1px solid rgb(220, 220, 220);
+    padding-top: 15px;
+    padding-bottom: 15px;
+    border: none;
+    box-shadow: 0px 1.5px 1.5px 1px;
+    border-radius: 0.5rem;
+    width: 100%;
+`;
+
+interface ToDoSearchProps {
+    myTodo: (content: string) => void;
+    items: {
+        id: number;
+        content: string;
+        createdDate: number;
+        isDone: boolean;
+    }[];
+    onDelete: (id: number) => void;
+}
+
+const ToDoSearch: React.FC<ToDoSearchProps> = ({ myTodo, items, onDelete }) => {
+    const [content, setContent] = useState<string>("");
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setContent(e.target.value);
+        myTodo(e.target.value);
+    };
+
     return (
         <div>
             <h3>Todo List📂</h3>
-            <Searchbar placeholder="검색어를 입력하세요" type="text" />
+            <Searchbar
+                placeholder="검색어를 입력하세요"
+                type="text"
+                value={content}
+                onChange={handleInputChange}
+            />
+            <DivContain>
+                {items.map((item) => (
+                    <ToDoItemBox
+                        key={item.id}
+                        id={item.id}
+                        content={item.content}
+                        createdDate={item.createdDate}
+                        isDone={item.isDone}
+                        onDelete={onDelete}
+                    />
+                ))}
+            </DivContain>
         </div>
     );
 };

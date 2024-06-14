5db3cf1ab11845751dbe2e8a12b984eb3e8db73e
sjndowair <wodnjsdl1125@naver.com>
2024-06-15 00:01:44 +0900

    add: 컴포넌트 추기

--- src/components/SecondTD/SecondEditor.tsx
+++ src/components/SecondTD/SecondEditor.tsx
@@ -1,69 +1,36 @@
 import { useState } from "react";
-import styled from "styled-components";
 import { FC } from "react";
+import SecondList from "./SecondList";
+import CreateToDo from "./CreateToDo";
 
-const InputContain = styled.form`
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    gap: 2.2rem;
-`;
-
-const TextAirear = styled.input`
-    width: 80%;
-    height: 5vw;
-    border: none;
-    border-radius: 1rem;
-    box-shadow: 0px 1px 2px 0px;
-    text-indent: 1rem;
-`;
-
-const AddBtn = styled.input`
-    border: none;
-    border-radius: 0.4rem;
-    background-color: #333;
-    outline: none;
-    color: #fff;
-    width: 15%;
-    height: 5vw;
-    font-size: 1rem;
-    box-shadow: 0px 3px 2px 0px;
-    transition: all 0.7s;
-
-    &:hover {
-        background-color: #fff;
-        color: #333;
-        border: solid 1px #333;
-    }
-`;
-
-interface secondEditProps {
-    onCreate: (content: string) => void;
+interface secondToDoProps {
+    id: number;
+    content: string;
+    isDone: boolean;
+    createDate: string;
 }
 
-const SecondEditor: FC<secondEditProps> = ({ onCreate }) => {
-    const [content, setContent] = useState("");
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (content.trim()) {
-            onCreate(content);
-            setContent("");
-        }
-    };
+const SecondToDo = () => {
+    const [todo, setTodo] = useState<secondToDoProps[]>([
+        {
+            id: 0,
+            content: "",
+            isDone: false,
+            createDate: "",
+        },
+    ]);
 
     return (
-        <div>
-            <h3>새로운 ToDo 작성하기</h3>
-            <InputContain action="" onSubmit={onSubmit}>
-                <TextAirear
-                    type="text"
-                    value={content}
-                    onChange={(e) => {
-                        setContent(e.currentTarget.value);
-                    }}
+        <div className="todoListContainer">
+            {todo.map((item) => (
+                <SecondList
+                    id={item.id}
+                    content={item.content}
+                    isDone={item.isDone}
+                    createDate={item.createDate}
                 />
-                <AddBtn type="submit" />
-            </InputContain>
+            ))}
+            <CreateToDo />
         </div>
     );
 };

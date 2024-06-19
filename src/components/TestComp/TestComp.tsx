fb9531f06a4cd59106ea2f5fd51fcbf28f90efda
sjndowair <wodnjsdl1125@naver.com>
2024-06-20 00:05:55 +0900

    fix: useState를 useReducer로 수정

--- src/components/TestComp/TestComp.tsx
+++ src/components/TestComp/TestComp.tsx
@@ -1,42 +1,112 @@
-import { SlowBuffer } from "buffer";
-import { useReducer, useState } from "react";
+import React, { KeyboardEvent, ChangeEvent, useReducer, useState } from "react";
+import styled from "styled-components";
 
-type ActionType =
-    | { type: "INCREASE"; data: number }
-    | { type: "DECREASE"; data: number }
-    | { type: "CLEAR"; data: number };
+const OtherBtn = styled.li`
+    list-style: none;
+`;
 
-type StateType = number;
-const reducer = (state: StateType, action: ActionType): StateType => {
+const FlexBox = styled.div`
+    display: flex;
+    justify-content: space-around;
+`;
+
+const FullContain = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+interface stateProps {
+    id: number;
+    content: string;
+    createTime: string;
+    isDone: boolean;
+}
+
+type actionType =
+    | { type: "ADD_TODO"; payload: stateProps }
+    | { type: "DELETED_TODO"; id: number }
+    | { type: "TOGGLE_TODO"; id: number };
+
+const reducer = (state: stateProps[], action: actionType): stateProps[] => {
     switch (action.type) {
-        case "INCREASE":
-            return state + action.data;
-        case "DECREASE":
-            return state - action.data;
-        case "CLEAR":
-            return 0;
+        case "ADD_TODO":
+            return [action.payload, ...state];
+        case "TOGGLE_TODO":
+            return state.map((todo) =>
+                todo.id === action.id ? { ...todo, isDone: !todo.isDone } : todo
+            );
+        case "DELETED_TODO":
+            return state.filter((todo) => todo.id !== action.id);
+        default:
+            return state;
     }
 };
-console.log(reducer);
 
-const TestComp = () => {
-    const [count, dispatch] = useReducer(reducer, 0);
+const TestComp: React.FC = () => {
+    const [todos, dispatch] = useReducer(reducer, []);
+    const [inputText, setInputText] = useState<string>("");
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.currentTarget.value);
+        console.log(e.currentTarget.value);
+    };
+
+    const handleEnterEvent = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            if (e.currentTarget.value === "") {
+                alert("글자를 입력해주세요");
+            } else {
+                addToDo();
+            }
+
+            console.log(e);
+        }
+    };
+
+    const addToDo = () => {
+        const newTodo: stateProps = {
+            id: Date.now(),
+            content: inputText,
+            isDone: false,
+            createTime: new Date().toLocaleDateString(),
+        };
+        dispatch({ type: "ADD_TODO", payload: newTodo });
+        setInputText("");
+    };
+
     return (
-        <div>
-            <h4>테스트 컴포넌트</h4>
-            <div>
-                <strong>{count}</strong>
-            </div>
-            <button onClick={() => dispatch({ type: "INCREASE", data: 1 })}>
-                +
-            </button>
-            <button onClick={() => dispatch({ type: "DECREASE", data: 1 })}>
-                -
-            </button>
-            <button onClick={() => dispatch({ type: "CLEAR", data: 0 })}>
-                clear
-            </button>
-        </div>
+        <FullContain>
+            <FlexBox>
+                <input
+                    type="text"
+                    onKeyDown={handleEnterEvent}
+                    value={inputText}
+                    onChange={handleInputChange}
+                />
+                <button onClick={addToDo}></button>
+            </FlexBox>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        <input
+                            type="checkbox"
+                            checked={todo.isDone}
+                            onChange={() =>
+                                dispatch({ type: "TOGGLE_TODO", id: todo.id })
+                            }
+                        />
+                        {todo.content}
+                        <button
+                            onClick={() =>
+                                dispatch({ type: "DELETED_TODO", id: todo.id })
+                            }
+                        >
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </FullContain>
     );
 };
 
